aef202d508c8b94a1523ffd926bcb334bdea896e
phuocleoceo <ht10082001@gmail.com>
2023-05-15 21:50:42 +0700

    Change Environment in DotEnv

--- src/configs/env.config.ts
+++ src/configs/env.config.ts
@@ -1,4 +1,6 @@
+const environment = import.meta.env.VITE_ENVIRONMENT
+
 export const EnvConfig = {
-  PLCBaseUrl: import.meta.env.VITE_PLC_BASE_URL ?? 'https://base.phuocleoceo.tech',
+  PLCBaseUrl: import.meta.env[`VITE_PLC_${environment}_URL`] ?? 'https://base.phuocleoceo.tech',
   HttpTimeout: import.meta.env.VITE_HTTP_TIMEOUT ? Number(import.meta.env.VITE_HTTP_TIMEOUT) : 10000
 } as const
