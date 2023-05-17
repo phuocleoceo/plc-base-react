2b19142c8b225fdabb1ca8ae048487c1e6b70dd1
phuocleoceo <ht10082001@gmail.com>
2023-05-18 00:22:02 +0700

    Media Base

---
+++ src/features/media/apis/index.ts
@@ -0,0 +1,1 @@
+export { default as MediaApi } from './mediaApi'
---
+++ src/features/media/apis/mediaApi.ts
@@ -0,0 +1,18 @@
+import { FileUploadResponse } from '~/features/media/models'
+import { HttpHelper } from '~/shared/helpers'
+
+const mediaApi = {
+  uploadFile(file: File, prefix?: string) {
+    const formData = new FormData()
+    formData.append('file', file)
+
+    return HttpHelper.post<FileUploadResponse>('upload-file', formData, {
+      params: { prefix }
+      //   headers: {
+      //     'Content-Type': 'multipart/form-data'
+      //   }
+    })
+  }
+}
+
+export default mediaApi
