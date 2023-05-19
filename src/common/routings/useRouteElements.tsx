7383ed6edca7609b31ae6461e768213686a43814
phuocleoceo <ht10082001@gmail.com>
2023-05-19 11:12:46 +0700

    Profile Base

--- src/common/routings/useRouteElements.tsx
+++ src/common/routings/useRouteElements.tsx
@@ -5,6 +5,7 @@ import { projectMemberRoute } from '~/features/projectMember/routes'
 import { invitationRoute } from '~/features/invitation/routes'
 import { adminUserRoute } from '~/features/admin/user/routes'
 import { projectRoute } from '~/features/project/routes'
+import { profileRoute } from '~/features/profile/routes'
 import { sprintRoute } from '~/features/sprint/routes'
 import { issueRoute } from '~/features/issue/routes'
 import { authRoute } from '~/features/auth/routes'
@@ -12,6 +13,7 @@ import { defaultRoute } from './defaultRoute'
 
 const routes: RouteObject[] = [
   ...authRoute,
+  ...profileRoute,
   ...projectRoute,
   ...invitationRoute,
   ...issueRoute,
