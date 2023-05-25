050a3cbb7ae9174353458e1a4378863d202a961b
phuocleoceo <ht10082001@gmail.com>
2023-05-25 17:22:42 +0700

    Projetc Route

--- src/common/routings/defaultRoute.tsx
+++ src/common/routings/defaultRoute.tsx
@@ -1,8 +1,19 @@
-import { RouteObject } from 'react-router-dom'
+import { Navigate, RouteObject } from 'react-router-dom'
 
 import { NotFound, AccessDenied } from '~/common/components'
+import { RequiredAuthenticatedRoute } from './guardRoute'
 
 export const defaultRoute: RouteObject[] = [
+  {
+    path: '/',
+    element: <RequiredAuthenticatedRoute />,
+    children: [
+      {
+        path: '',
+        element: <Navigate to='/project' />
+      }
+    ]
+  },
   {
     path: '/access-denied',
     element: <AccessDenied />
--- src/features/project/routes/projectRoute.tsx
+++ src/features/project/routes/projectRoute.tsx
@@ -1,16 +1,22 @@
 import { RouteObject } from 'react-router-dom'
 
-import { RequiredAuthenticatedRoute } from '~/common/routings'
+import { RequiredAuthenticatedRoute } from '~/common/routings/guardRoute'
 import { ProjectList } from '~/features/project/pages'
 import { HomeLayout } from '~/features/home/layouts'
 
 export const projectRoute: RouteObject[] = [
   {
-    path: '/',
-    element: (
-      <HomeLayout>
-        <ProjectList />
-      </HomeLayout>
-    )
+    path: '/project',
+    element: <RequiredAuthenticatedRoute />,
+    children: [
+      {
+        path: '',
+        element: (
+          <HomeLayout>
+            <ProjectList />
+          </HomeLayout>
+        )
+      }
+    ]
   }
 ]
