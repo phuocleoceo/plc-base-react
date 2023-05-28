6a8c782aa5d4d4f4351e7f3ba65577d1245bc8b7
phuocleoceo <ht10082001@gmail.com>
2023-05-28 22:42:48 +0700

    Style for paginate

--- src/common/components/Pagination.tsx
+++ src/common/components/Pagination.tsx
@@ -16,27 +16,30 @@ export default function Pagination(props: Props) {
     onChangePage(selectedItem.selected + 1)
   }
 
+  if (totalRecords === 0) return null
+
   return (
     <div className='flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6'>
       <div>
-        <p className='text-sm text-gray-700'>total has {totalRecords} results</p>
+        <p className='text-black'>total has {totalRecords} results</p>
       </div>
 
       <ReactPaginate
         pageCount={totalPages}
         onPageChange={handlePageClick}
         breakLabel='...'
+        breakClassName='px-2 py-1'
         containerClassName='flex justify-center'
-        pageClassName='inline-block mx-1'
+        pageClassName='mx-2 mt-1'
         previousLabel={
           <Icon className='text-secondary group-hover:text-white' fontSize={20} icon='fa-solid:angle-left' />
         }
         previousClassName='px-2 py-1 border rounded-lg hover:bg-gray-200'
         nextLabel={<Icon className='text-secondary group-hover:text-white' fontSize={20} icon='fa-solid:angle-right' />}
         nextClassName='px-2 py-1 border rounded-lg hover:bg-gray-200'
-        activeClassName='font-bold'
-        breakClassName='px-2 py-1'
+        activeClassName='font-bold text-chakra-blue'
         disabledClassName='opacity-50 cursor-not-allowed'
+        renderOnZeroPageCount={null}
       />
     </div>
   )
