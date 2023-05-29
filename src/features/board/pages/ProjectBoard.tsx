4f0bb53873e437844cc048f48ef59026b6f69965
phuocleoceo <ht10082001@gmail.com>
2023-05-30 01:10:53 +0700

    Setup filter bar

--- src/features/board/pages/ProjectBoard.tsx
+++ src/features/board/pages/ProjectBoard.tsx
@@ -1,3 +1,17 @@
+import { useParams } from 'react-router-dom'
+import { useState } from 'react'
+
+import { FilterBar } from '~/features/board/components'
+
 export default function ProjectBoard() {
-  return <div className='ml-10'>Board</div>
+  const projectId = Number(useParams().projectId)
+
+  const [isDragDisabled, setIsDragDisabled] = useState(false)
+
+  return (
+    <div className='mt-6 flex grow flex-col px-8 sm:px-10'>
+      <h1 className='mb-4 text-xl font-semibold text-c-text'>kanban_board</h1>
+      <FilterBar maxMemberDisplay={4} {...{ projectId, setIsDragDisabled }} />
+    </div>
+  )
 }
