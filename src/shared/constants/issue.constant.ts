ad3f1f38b14d01d42d4c3904dc2e523857c448ed
phuocleoceo <ht10082001@gmail.com>
2023-05-31 15:02:26 +0700

    Fix icon in dev env

--- src/shared/constants/issue.constant.ts
+++ src/shared/constants/issue.constant.ts
@@ -1,12 +1,20 @@
+import CodingTaskIcon from '~/assets/svg/coding-task.svg'
+import BugIcon from '~/assets/svg/bug.svg'
+import UserStoryIcon from '~/assets/svg/user-story.svg'
+import LowIcon from '~/assets/svg/low.svg'
+import MediumIcon from '~/assets/svg/medium.svg'
+import HighIcon from '~/assets/svg/high.svg'
+import CriticalIcon from '~/assets/svg/critical.svg'
+
 export const IssueType = [
-  { text: 'coding_task', value: 'coding_task', icon: '/src/assets/svg/coding-task.svg' },
-  { text: 'bug', value: 'bug', icon: '/src/assets/svg/bug.svg' },
-  { text: 'user_story', value: 'user_story', icon: '/src/assets/svg/user-story.svg' }
+  { text: 'coding_task', value: 'coding_task', icon: CodingTaskIcon },
+  { text: 'bug', value: 'bug', icon: BugIcon },
+  { text: 'user_story', value: 'user_story', icon: UserStoryIcon }
 ]
 
 export const IssuePriority = [
-  { text: 'low', value: 'low', icon: '/src/assets/svg/low.svg' },
-  { text: 'medium', value: 'medium', icon: '/src/assets/svg/medium.svg' },
-  { text: 'high', value: 'high', icon: '/src/assets/svg/high.svg' },
-  { text: 'critical', value: 'critical', icon: '/src/assets/svg/critical.svg' }
+  { text: 'low', value: 'low', icon: LowIcon },
+  { text: 'medium', value: 'medium', icon: MediumIcon },
+  { text: 'high', value: 'high', icon: HighIcon },
+  { text: 'critical', value: 'critical', icon: CriticalIcon }
 ]
