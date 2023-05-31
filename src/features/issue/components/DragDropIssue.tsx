a0568e74c688177367412c7275ae323222f08b4b
phuocleoceo <ht10082001@gmail.com>
2023-05-31 14:17:30 +0700

    Issue Priority Icon

--- src/features/issue/components/DragDropIssue.tsx
+++ src/features/issue/components/DragDropIssue.tsx
@@ -1,6 +1,6 @@
 import { IssueInBoard } from '~/features/issue/models'
 import { DraggableWrapper } from '~/common/components'
-import { IssueType } from '~/shared/constants'
+import { IssueHelper } from '~/shared/helpers'
 import { useShowing } from '~/common/hooks'
 
 type Props = IssueInBoard & {
@@ -9,13 +9,9 @@ type Props = IssueInBoard & {
 }
 
 export default function DragDropIssue(props: Props) {
-  const { idx, isDragDisabled, id, title, type } = props
+  const { idx, isDragDisabled, id, title, type, priority } = props
 
   const { isShowing: isShowingIssueDetail, toggle: toggleIssueDetail } = useShowing()
-
-  const getIssueType = (typeValue: string) => {
-    return IssueType.find((it) => it.value === typeValue)
-  }
 
   return (
     <>
@@ -27,16 +23,22 @@ export default function DragDropIssue(props: Props) {
       >
         <div onClick={toggleIssueDetail} onKeyDown={toggleIssueDetail} tabIndex={id} role='button'>
           <span className=''>{title}</span>
+
           <div className='mt-[10px] flex items-center justify-between'>
             <div className='mb-1 flex items-center gap-3'>
-              <img className='h-[18px] w-[18px]' src={getIssueType(type)?.icon} alt={getIssueType(type)?.text} />
-              {/* <img className='h-[18px] w-[18px]' src={icon} alt={text} /> */}
-              {/* {comments > 0 && (
-                <span className='block w-6 rounded-md bg-c-2 text-center text-[13px]'>
-                  {comments}
-                </span>
-              )} */}
+              <img
+                className='h-[18px] w-[18px]'
+                src={IssueHelper.getIssueType(type)?.icon}
+                alt={IssueHelper.getIssueType(type)?.text}
+              />
+
+              <img
+                className='h-[18px] w-[18px]'
+                src={IssueHelper.getIssuePriority(priority)?.icon}
+                alt={IssueHelper.getIssuePriority(priority)?.text}
+              />
             </div>
+
             {/* <AssignedMembers assignees={assignees} members={members} /> */}
           </div>
         </div>
