e8fba2abfe37262ac54ee7fd0925bb87acfee52c
phuocleoceo <ht10082001@gmail.com>
2023-06-02 00:09:27 +0700

    Backlog list

--- src/features/backlog/pages/ProjectBacklog.tsx
+++ src/features/backlog/pages/ProjectBacklog.tsx
@@ -1,64 +1,98 @@
 import { DragDropContext, DropResult } from '@hello-pangea/dnd'
 import { useParams } from 'react-router-dom'
-import { useContext, useState } from 'react'
+import { useContext } from 'react'
 
-import { DragDropStatus } from '~/features/projectStatus/components'
-import { ProjectStatusApi } from '~/features/projectStatus/apis'
-import { GetIssuesInBoardParams } from '~/features/issue/models'
-import { DroppableWrapper } from '~/common/components'
+import { Avatar, DraggableWrapper, DroppableWrapper } from '~/common/components'
 import { useQuery } from '@tanstack/react-query'
 import { IssueApi } from '~/features/issue/apis'
+import { IssueHelper } from '~/shared/helpers'
 import { AppContext } from '~/common/contexts'
 import { QueryKey } from '~/shared/constants'
+import { useShowing } from '~/common/hooks'
 
 export default function ProjectBacklog() {
   const projectId = Number(useParams().projectId)
 
   const { isAuthenticated } = useContext(AppContext)
-  const [isDragDisabled, setIsDragDisabled] = useState(false)
+  const { isShowing: isShowingIssueDetail, toggle: toggleIssueDetail } = useShowing()
 
-  // ---------------------Project Status---------------------
-  const { data: projectStatusData } = useQuery({
-    queryKey: [QueryKey.ProjectStatuses, projectId],
-    queryFn: () => ProjectStatusApi.getProjectStatus(projectId),
+  const { data: backlogData } = useQuery({
+    queryKey: [QueryKey.IssueInBacklog, projectId],
+    queryFn: () => IssueApi.getIssuesInBacklog(projectId),
     enabled: isAuthenticated,
     keepPreviousData: true,
     staleTime: 1 * 60 * 1000
   })
 
-  const projectStatuses = projectStatusData?.data.data
+  const issuesBacklog = backlogData?.data.data
 
-  // ----------------------Issue----------------------------
-  const [issueParams, setIssueParams] = useState<GetIssuesInBoardParams>({
-    searchValue: '',
-    assignees: ''
-  })
-
-  const handleChangeIssueParams = (key: string, value: string) => {
-    setIssueParams({
-      ...issueParams,
-      [key]: value
-    })
-  }
-
-  const { data: issueData } = useQuery({
-    queryKey: [QueryKey.IssueInBoard, projectId, issueParams],
-    queryFn: () => IssueApi.getIssuesInBoard(projectId, issueParams),
-    enabled: isAuthenticated,
-    keepPreviousData: true,
-    staleTime: 1 * 60 * 1000
-  })
-
-  const issues = issueData?.data.data
-
-  const getIssuesByStatusId = (statusId?: number) => {
-    return issues?.find((i) => i.projectStatusId === statusId)?.issues ?? []
-  }
-
-  // ----------------------Drag Drop----------------------------
   const handleDragEnd = ({ type, source, destination }: DropResult) => {
     console.log(type, source, destination)
   }
 
-  return <div className='mt-6 flex grow flex-col px-8 sm:px-10'>abc</div>
+  return (
+    <div className='mt-6 flex grow flex-col px-8 sm:px-10'>
+      <h1 className='mb-4 text-xl font-semibold text-c-text'>backlog</h1>
+
+      {issuesBacklog && issuesBacklog?.length > 0 && (
+        <div className='mb-5 flex min-w-max grow items-start'>
+          <DragDropContext onDragEnd={handleDragEnd}>
+            <DroppableWrapper
+              type='issueBacklog'
+              className='items-start'
+              droppableId='board-central'
+              direction='horizontal'
+            >
+              {issuesBacklog.map((issue, idx) => (
+                <DraggableWrapper
+                  key={idx}
+                  className='w-[60rem] border-[1px] p-[0.1rem] mb-[0.2px]'
+                  index={idx}
+                  draggableId={`projectStatus-${issue.id}`}
+                  isDragDisabled={false}
+                >
+                  <div onClick={toggleIssueDetail} onKeyDown={toggleIssueDetail} tabIndex={issue.id} role='button'>
+                    <div className='flex items-center justify-between'>
+                      <div className='flex items-center gap-3'>
+                        <img
+                          className='h-[18px] w-[18px] ml-2'
+                          src={IssueHelper.getIssueType(issue.type)?.icon}
+                          alt={IssueHelper.getIssueType(issue.type)?.text}
+                        />
+                        <span className=''>{issue.title}</span>
+                      </div>
+
+                      <div className='ml-7 flex'>
+                        {issue.assigneeId ? (
+                          <Avatar
+                            key={issue.assigneeId}
+                            src={issue.assigneeAvatar}
+                            name={issue.assigneeName}
+                            style={{ zIndex: 0 }}
+                            className='pointer-events-none -ml-2 h-7 w-7 border-2'
+                          />
+                        ) : (
+                          <div className='h-7 w-7'></div>
+                        )}
+
+                        <img
+                          className='h-[18px] w-[18px] ml-2 mt-1'
+                          src={IssueHelper.getIssuePriority(issue.priority)?.icon}
+                          alt={IssueHelper.getIssuePriority(issue.priority)?.text}
+                        />
+
+                        <div className='ml-2 text-sm rounded-full border border-transparent bg-gray-100 px-[3px] flex items-center'>
+                          {issue.projectStatusName}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </DraggableWrapper>
+              ))}
+            </DroppableWrapper>
+          </DragDropContext>
+        </div>
+      )}
+    </div>
+  )
 }
