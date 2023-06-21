be0797fcef005ccb5bca8519ccc1fe34065d3549
Truong Minh Phuoc <ht10082001@gmail.com>
2023-06-21 13:28:48 +0700

    feat/issue-attach-file (#30)
    * Backup
    
    * Upload image done

--- src/common/components/RichTextInput.tsx
+++ src/common/components/RichTextInput.tsx
@@ -1,6 +1,10 @@
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
 import { CKEditor } from '@ckeditor/ckeditor5-react'
 import { Controller } from 'react-hook-form'
+import { toast } from 'react-toastify'
+
+import { TranslateHelper } from '~/shared/helpers'
+import { MediaApi } from '~/features/media/apis'
 
 interface Prop {
   control?: any
@@ -10,6 +14,30 @@ interface Prop {
 
 export default function RichTextInput(props: Prop) {
   const { control, controlField, defaultValue } = props
+
+  function uploadAdapter(loader: any) {
+    return {
+      upload: () => {
+        return loader.file.then(async (file: File) => {
+          try {
+            const imageUploadResponse = await MediaApi.uploadFile(file)
+            const imageUrl = imageUploadResponse?.data.data || ''
+            return {
+              default: imageUrl
+            }
+          } catch {
+            toast.error(TranslateHelper.translate('upload_file_fail'))
+          }
+        })
+      }
+    }
+  }
+
+  function uploadPlugin(editor: any) {
+    editor.plugins.get('FileRepository').createUploadAdapter = (loader: any) => {
+      return uploadAdapter(loader)
+    }
+  }
 
   if (!control || !controlField) return null
 
@@ -24,6 +52,9 @@ export default function RichTextInput(props: Prop) {
           data={defaultValue}
           onChange={(_, editor) => {
             onChange(editor.getData())
+          }}
+          config={{
+            extraPlugins: [uploadPlugin]
           }}
         />
       )}
